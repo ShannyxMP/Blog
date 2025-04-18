6f35a3ece3d144d295ef5f2bf9a4dbc638c60a2b
ShannyxMP <shannyxmp@gmail.com>
2025-04-18 21:35:03 +1000

    feat: add post creation features

--- index.js
+++ index.js
@@ -4,12 +4,31 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+let posts = [];
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded());
 
 app.get("/", (req, res) => {
   console.log("You are on the homepage.");
-  res.render("index.ejs");
+  res.render("index.ejs", { posts: posts });
+});
+
+app.get("/add-post", (req, res) => {
+  console.log("You are on the 'add-post' page.");
+  res.render("add-post.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  let post = {
+    title: req.body["title"],
+    content: req.body["content"],
+  };
+  posts.push(post);
+  console.log("Post added.");
+
+  res.render("index.ejs", { posts: posts });
+});
 });
 
 // Start the server:
