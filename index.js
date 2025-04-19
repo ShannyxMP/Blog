c9bad9096a7c4f93f4af28e05d9d08fe619b4ca3
ShannyxMP <shannyxmp@gmail.com>
2025-04-19 11:27:49 +1000

    refactor: handle invalid entries and cleanup code

--- index.js
+++ index.js
@@ -4,21 +4,25 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-let posts = [];
+let posts = []; // To store blog posts
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded());
+// Middlewares:
+app.use(express.static("public")); // Handle static files
+app.use(bodyParser.urlencoded()); // Parse URL-encoded form data
 
+// Route: Homepage - displays blog posts
 app.get("/", (req, res) => {
-  console.log("You are on the homepage.");
+  console.log("On the homepage.");
   res.render("index.ejs", { posts: posts });
 });
 
+// Route: Add post page - form for creating new post
 app.get("/add-post", (req, res) => {
-  console.log("You are on the 'add-post' page.");
+  console.log("On 'add-post' page.");
   res.render("add-post.ejs");
 });
 
+// Route: Handle form submission to add new post
 app.post("/submit", (req, res) => {
   let post = {
     title: req.body["title"],
@@ -27,9 +31,10 @@ app.post("/submit", (req, res) => {
   posts.push(post);
   console.log("Post added.");
 
-  res.render("index.ejs", { posts: posts });
+  res.redirect("/");
 });
 
+// Route: Edit post page - shows form to edit chosen post
 app.get("/edit-post/:postId", (req, res) => {
   const postIndex = req.params.postId;
   const post = posts[postIndex];
@@ -37,25 +42,36 @@ app.get("/edit-post/:postId", (req, res) => {
   res.render("edit-post.ejs", { post: post, index: postIndex });
 });
 
-app.post("/", (req, res) => {
+// Route: Handle form submission to update a post
+app.post("/edit-post/:postId", (req, res) => {
   const postIndex = req.body["index"];
 
-  posts[postIndex].title = req.body["title"];
-  posts[postIndex].content = req.body["content"];
+  if (!posts[postIndex]) {
+    console.log("No post found at that index.");
+  } else {
+    posts[postIndex].title = req.body["title"];
+    posts[postIndex].content = req.body["content"];
+    console.log("Editted post saved.");
+  }
 
-  console.log("Editted post saved.");
-  // res.render("index.ejs", { posts: posts });
   res.redirect("/");
 });
 
+// Route: Delete a post by its index
 app.post("/delete-post/:postId", (req, res) => {
   const postIndex = req.params.postId;
-  posts.splice(postIndex, 1);
-  console.log(`Post ${postIndex} deleted`);
+
+  if (!posts[postIndex]) {
+    console.log("No post found at this index.");
+  } else {
+    posts.splice(postIndex, 1);
+    console.log(`Post ${postIndex} deleted`);
+  }
+
   res.redirect("/");
 });
 
-// Start the server:
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
